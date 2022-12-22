afabb201e3106c679f1f533c2f7c698133f99667
sifatimah <sfatimah0521@gmail.com>
2022-12-22 14:33:59 +0700

    Siti_fatimah: Menambahkan Footer

---
+++ application/views/layouts/_footer.php
@@ -0,0 +1,79 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+<link rel="stylesheet" href="<?= base_url("/assets/css/footer.css") ?>">
+
+<!-- <h1><center></center></h1> -->
+
+<!--footer starts from here-->
+<footer class="footer">
+<div class="container bottom_border">
+<div class="row">
+<div class=" col-sm-4 col-md col-sm-4  col-12 col">
+<h5 class="headin5_amrc col_white_amrc pt2">Find us</h5>
+<!--headin5_amrc-->
+<p class="mb10">FJS Bucket Shop Merupakan sebuah toko online penjualan aneka macam bucket yang berada di kota bandar lampung dan berdiri sejak tahuan 2020</p>
+<p><i class="fa fa-location-arrow"></i> Labuhan Ratu, Bandar Lampung </p>
+<p><i class="fa fa-phone"></i>  081990670987  </p>
+<p><i class="fa fa fa-envelope"></i> fjsbucketshop@gmail.com  </p>
+
+
+</div>
+
+
+<div class=" col-sm-4 col-md  col-6 col">
+<h5 class="headin5_amrc col_white_amrc pt2">Informasi</h5>
+<!--headin5_amrc-->
+<ul class="footer_ul_amrc">
+<li><a href="http://webenlance.com">Tentang Kami</a></li>
+<li><a href="http://webenlance.com">Syarat & Ketentuan</a></li>
+<li><a href="http://webenlance.com">Kebijakan Privasi</a></li>
+<li><a href="http://webenlance.com">Program Reseller</a></li>
+<li><a href="http://webenlance.com">Semua Koleksi</a></li>
+</ul>
+<!--footer_ul_amrc ends here-->
+</div>
+
+
+<div class=" col-sm-4 col-md  col-6 col">
+<h5 class="headin5_amrc col_white_amrc pt2">Bantuan</h5>
+<!--headin5_amrc-->
+<ul class="footer_ul_amrc">
+<li><a href="http://webenlance.com">Metode Pembayaran</a></li>
+<li><a href="http://webenlance.com">Kontak Person</a></li>
+<li><a href="http://webenlance.com">Pengiriman</a></li>
+<li><a href="http://webenlance.com">FAQ</a></li>
+</ul>
+<!--footer_ul_amrc ends here-->
+</div>
+
+
+<div class=" col-sm-4 col-md  col-12 col">
+<h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
+<!--headin5_amrc ends here-->
+
+<ul class="footer_ul2_amrc">
+<li><a href="http://webenlance.com"><i class="fab fa-facebook-f"></i></a> FJS Bucket Shop </li> <br>
+<li><a href="http://webenlance.com"><i class="fab fa-twitter"></i></a> @fjs_bucket </li> <br>
+<li><a href="http://webenlance.com"><i class="fab fa-linkedin"></i></a> FJS Bucket Shop </li> <br>
+<li><a href="http://webenlance.com"><i class="fab fa-instagram"></i></a> @fjs_bucketshop </li>
+</ul>
+<!--footer_ul2_amrc ends here-->
+</div>
+</div>
+</div>
+
+<p class="text-center">Copyright @2022 | Designed With by <a href="#">FJS Bucket Shop</a></p>
+
+</div>
+
+</footer>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
